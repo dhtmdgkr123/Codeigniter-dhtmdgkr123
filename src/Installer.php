63f44b64a45e679b99af53e425b5c8316305e344
dhtmdgkr <osh12201@gmail.com>
2019-12-25 19:55:22 +0900

    Dev : Self Delete
    Self delete

--- src/Installer.php
+++ src/Installer.php
@@ -1,46 +1,54 @@
 <?php
 namespace dhtmdgkr123\CodeIgniter;
 use Composer\Script\Event;
-if(! class_exists('AfterInstall')) {
-    class AfterInstall
+class AfterInstall
+{
+    public static function postInstall(Event $event = null) : void
     {
-        public static function postInstall(Event $event = null) : void
-        {
-            self::removeUnUsedVendor();
-        }
-        private static function removeUnUsedVendor() : void
-        {
-            $base = realpath('../vendor/codeigniter/framework');
-            if ($base) {
-                $base .= '/';
-                $unusedFolders = [
-                    'application',
-                    'user_guide',
-                ];
-                $unusedFiles = [
-                    '.editorconfig',
-                    '.gitignore',
-                    'composer.json',
-                    'index.php',
-                    'readme.rst',
-                ];
-                
-                foreach ($unusedFiles as $file) {
-                    if (file_exists($base.$file)) {
-                        unlink($base.$file);
-                    }
+        echo 'Hello';
+        // self::removeUnUsedVendor();
+        // self::editComposer();
+    }
+    private static function selfDelete() : void
+    {
+        $sorcePath = realpath('../composer.json.bak');
+        $distPath = realpath('../composer.json');
+        copy($sorcePath, $distPath);
+        unlink($sorcePath);
+
+    }
+    private static function removeUnUsedVendor() : void
+    {
+        $base = realpath('../vendor/codeigniter/framework');
+        if ($base) {
+            $base .= '/';
+            $unusedFolders = [
+                'application',
+                'user_guide',
+            ];
+            $unusedFiles = [
+                '.editorconfig',
+                '.gitignore',
+                'composer.json',
+                'index.php',
+                'readme.rst',
+            ];
+            
+            foreach ($unusedFiles as $file) {
+                if (file_exists($base.$file)) {
+                    unlink($base.$file);
                 }
-                foreach ($unusedFolders as $folder) {
-                    $dirIterator = new \RecursiveIteratorIterator(
-                        new \RecursiveDirectoryIterator($base.$folder, \RecursiveDirectoryIterator::SKIP_DOTS),
-                        \RecursiveIteratorIterator::CHILD_FIRST
-                    );
-                    foreach ($dirIterator as $info) {
-                        $todo = ($info->isDir() ? 'rmdir' : 'unlink');
-                        $todo($info->getRealPath());
-                    }
-                    rmdir($base.$folder);
+            }
+            foreach ($unusedFolders as $folder) {
+                $dirIterator = new \RecursiveIteratorIterator(
+                    new \RecursiveDirectoryIterator($base.$folder, \RecursiveDirectoryIterator::SKIP_DOTS),
+                    \RecursiveIteratorIterator::CHILD_FIRST
+                );
+                foreach ($dirIterator as $info) {
+                    $todo = ($info->isDir() ? 'rmdir' : 'unlink');
+                    $todo($info->getRealPath());
                 }
+                rmdir($base.$folder);
             }
         }
     }
