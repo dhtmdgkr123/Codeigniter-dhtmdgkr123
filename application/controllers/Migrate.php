1c384966ac0435cfa69039baa892b7fad23f2a8a
dhtmdgkr123 <osh12201@gmail.com>
2020-01-09 12:11:12 +0900

    Fix : Comment, json header

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -1,25 +1,33 @@
 <?php
 if(! class_exists('Migrate')) {
     defined('BASEPATH') or exit('No direct script access allowed');
+    /**
+     * DB Migrate File Generator
+     * 
+     * @copyright  2020 dhtmdgkr123
+     * @version    Release: @1.0.1@
+     * @since      1.0.1
+     */
     class Migrate extends CI_Controller
     {
         public function __construct()
         {
             parent::__construct();
+
             if (! $this->input->is_cli_request()) {
                 show_404();
             } else {
                 $this->load->library('migration');
             }
         }
+        private function printLn(string $echoStr) : void
+        {
+            echo sprintf("%s\n", $echoStr);
+        }
         public function version(string $version) : void 
         {
             $migrateVer = $this->migration->version($version);
-            if (! $migrateVer) {
-                echo $this->migration->error_string();
-            } else {
-                echo 'success';
-            }
+            $this->printLn((! $migrateVer) ? $this->migration->error_string() : 'Success '.$version.'  migrate');
         }
         public function generate(string $name = '') : void
         {
@@ -27,15 +35,15 @@ if(! class_exists('Migrate')) {
                 'className' => null
             ];
             if (! $name) {
-                echo 'define migrate name';
+                $this->printLn('define migrate name');
                 return;
             }
             if (! preg_match('/^[a-z_]+$/i', $name)) {
                 if (strlen($name) < 4) {
-                    echo 'migate name is upper 4 character';
+                    $this->printLn('migate name is upper 4 character');
                     return;
                 }
-                echo 'character is must be a-z';
+                $this->printLn('character is must be a-z');
                 return;
             }
             $fileName = date('YmdHis') . '_' . $name . '.php';
